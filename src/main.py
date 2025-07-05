df9670ef1188f4d3dd96e402ae2fe9ee33aaf5c1
Zobiii <lechnazobi@gmail.com>
2025-07-05 03:41:21 +0200

    QoL changes for files integration

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 from ui.main_window import ToDoWindow
+import os
 
 def main():
     print("[Main] Starting the application...")
     app = QApplication([])
-    app.setWindowIcon(QIcon("c:/Users/lechn/Desktop/todo_app/assets/notepad_icon.png"))
+    
+    base_dir = os.path.dirname(__file__)
+    icon_path = os.path.join(base_dir, "../assets/notepad_icon.png")
+    app.setWindowIcon(QIcon(icon_path))
+
     window = ToDoWindow()
     window.show()
     app.exec()
--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -2,14 +2,18 @@ import json
 import os
 
 class FileHandler:
-    def __init__(self, filename="tasks.json"):
-        self.files_dir = os.path.join(os.path.dirname(__file__), '..', 'files')
-        self.filename = os.path.join(self.files_dir, filename)
+    def __init__(self):
+        documents_folder = os.path.join(os.path.expanduser("~"), "Documents")
+        self.app_folder = os.path.join(documents_folder, "ToDo_App")  # Unterordner "ToDo_App"
+        self.filename = os.path.join(self.app_folder, "tasks.json")  # Pfad zur tasks.json
 
-        os.makedirs(self.files_dir, exist_ok=True)
+        # Überprüfen, ob der Ordner existiert, ansonsten erstellen
+        if not os.path.exists(self.app_folder):
+            print(f"[FileHandler] Creating folder: {self.app_folder}")
+            os.makedirs(self.app_folder)
 
     def load_tasks(self):
-        print(f"[FileHandler] Loading tasks from file {self.filename}")
+        print(f"[FileHandler] Loading tasks from file: {self.filename}")
         if os.path.exists(self.filename):
             try:
                 with open(self.filename, "r", encoding="utf-8") as f:
